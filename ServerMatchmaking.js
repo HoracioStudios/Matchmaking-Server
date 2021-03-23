eed21ddbcf1d2b460c3d524e4f8e310d2a9a0c2d
Posna <jomart17@ucm.es>
2021-03-23 13:50:35 +0100

    Codigo ServerMatchmaking ligeramente comentado

--- ServerMatchmaking.js
+++ ServerMatchmaking.js
@@ -4,13 +4,13 @@ const server = express();
 server.use(express.json())
 const port = 25565;
 
-
+// Prueba de que el servidor esta operativo
 server.get('/prueba/get', (req, res) => {
   console.log(`Se hizo get`);
-  return res.send("Hola get");
+  return res.send("Prueba get");
 })
 
-// Comprobacion de la version actual del juego
+// Comprobación de la versión actual del juego
 // antes de hacer logIn
 server.get('/version', (req, res) => {
   var version = "1.0.0";
@@ -24,6 +24,8 @@ server.post('/prueba/post', (req, res) => {
   return res.send(req.body);
 })
 
+// Registro de un nuevo jugador
+// parámetros json: username, email, password
 server.post('/signin', (req, res) => {
   var username = req.body.username;
   var email = req.body.email;
@@ -31,11 +33,11 @@ server.post('/signin', (req, res) => {
   console.log(`Player ${username} is trying to sign in`);
 
   // Devuelve el id del jugador o un 
-  // codigo de error (numeros negativos)
+  // código de error (números negativos)
   return res.send("1");
 })
 
-
+// Se usa para hacer log in de los jugadores
 server.post('/login', (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
@@ -47,6 +49,8 @@ server.post('/login', (req, res) => {
   return res.send("1");
 })
 
+// Inicio de la cola. 
+// parametros json: id
 server.post('/startQueue', (req, res) => {
   var id = req.body.id;
   console.log(`Player ${id} is looking for a game`);
@@ -57,6 +61,8 @@ server.post('/startQueue', (req, res) => {
   return res.send("1");
 })
 
+// Metodo llamado para cancelar la cola del jugador
+// y eliminarlo de la lista de jugadores online
 server.post('/cancelQueue', (req, res) => {
   var id = req.body.id;
   console.log(`Player ${id} canceled the queue`);
